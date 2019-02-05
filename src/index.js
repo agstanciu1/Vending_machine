b517f497f8f4c0d179e21a457d7dc2ac3896a3ef
agstanciu1 <35116455+agstanciu1@users.noreply.github.com>
2019-02-05 14:02:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 
 import VendingMachine from './components/VendingMachine';
 import reducers from './reducers';
 
 ReactDOM.render(
-	<Provider store = {createStore(reducers, applyMiddleware(thunk))}>
+	<Provider store = {createStore(reducers)}>
 	<VendingMachine />
 	</Provider>,
 	 document.getElementById('root'));
